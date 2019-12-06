140395f3037ecdc228b99731627307bdcb6b33c6
Huy Nguyen <huynguyen@SGH034RY6T.kms.com.vn>
2019-12-06 15:59:27 +0700

    alert-delay

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,7 +1,17 @@
 if (window.pageId === 'alert.html') {
   var alertBtn = $('#alert');
+  var alertDelayBtn = $('#alert-delay');
 
   alertBtn.on("click", (e) => {
-    alert("This is an alert box")
+    alert("This is an alert box.")
   });
+
+  alertDelayBtn.on("click", (e) => {
+    setTimeout(
+      function() {
+        alert("This is an alert box that will show up after 10 seconds.")
+      },
+      10000
+    );
+  })
 }
